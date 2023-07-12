0c67d6ebd32c09587929b87990d3d17db5af0535
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2023-07-13 02:42:19 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,7 +21,7 @@ import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@/components/ui/hover-card";
+} from "@/components/UI/hover-card";
 import { Divider } from "primereact/divider";
 
 const Header = (props) => {
